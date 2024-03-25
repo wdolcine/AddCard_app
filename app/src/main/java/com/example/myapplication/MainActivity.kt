2a69e0dc5c419b79c5ed9a37b090ff687d2285b9
wdolcine <dolcinemarcarthur01@gmail.com>
2024-03-25 19:15:52 -0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
+        val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        val crossMain = findViewById<View>(R.id.imageButton3)
+
+        flashcardAnswer.visibility = View.INVISIBLE
+
+
+        flashcardQuestion.setOnClickListener {
+            flashcardQuestion.visibility = View.INVISIBLE
+            flashcardAnswer.visibility = View.VISIBLE
+        }
+        flashcardAnswer.setOnClickListener {
+            flashcardQuestion.visibility = View.VISIBLE
+            flashcardAnswer.visibility = View.INVISIBLE
+        }
+
+        crossMain.setOnClickListener {
+            val intent = Intent(this,AddCardActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
